c3efb9db1fea88bb80f96db548de79852857e9e3
doanhuy <huy.dn@teko.vn>
2023-03-16 13:07:36 +0700

    fix 4

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,6 @@
 import os
+
+import sqlalchemy
 from dotenv import load_dotenv
 from pydantic import BaseSettings
 
@@ -8,7 +10,17 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 db_name = "postgres"
 db_user = "postgres"
 db_password = "140fm993"
-host = '/cloudsql/{}'.format("emo-server-380518:us-central1:emo-posgresql")
+# host = '/cloudsql/{}'.format("emo-server-380518:us-central1:emo-posgresql")
+host = '34.29.12.24:5432'
+connection_name = "emo-server-380518:us-central1:emo-posgresql"
+
+url = sqlalchemy.engine.url.URL.create(
+    drivername="postgresql+psycopg2",
+    username=db_user,
+    password=db_password,
+    database=db_name,
+    query={"host": "{}/{}".format("/cloudsql", connection_name)},
+)
 
 
 class Settings(BaseSettings):
@@ -16,7 +28,7 @@ class Settings(BaseSettings):
     SECRET_KEY = '123456'
     API_PREFIX = ''
     BACKEND_CORS_ORIGINS = ['*']
-    DATABASE_URL = f'postgresql+psycopg2://{db_user}:{db_password}@{host}/{db_name}'
+    DATABASE_URL = url
     DATABASE_URL_UNIX = 'postgresql+psycopg2://postgres:140fm993@/postgres?unix_sock=/var/lib/postgresql'
     ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * 60 * 24 * 7  # Token expired after 7 days
     SECURITY_ALGORITHM = 'HS256'
