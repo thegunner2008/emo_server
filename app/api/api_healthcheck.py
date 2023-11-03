65fa152cd9cfc1c796f3fd5e192facabcc003739
doanhuy <huy.dn@teko.vn>
2023-11-03 17:11:08 +0700

    add new version

--- app/api/api_healthcheck.py
+++ app/api/api_healthcheck.py
@@ -7,4 +7,4 @@ router = APIRouter()
 
 @router.get("", response_model=ResponseSchemaBase)
 async def get():
-    return {"message": "Health check success - 10/09"}
+    return {"message": "Health check success - 03/11"}
