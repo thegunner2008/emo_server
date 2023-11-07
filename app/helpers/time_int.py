9f14afbfbc4c58a950cea9e139eb21185e70a5aa
doanhuy <huy.dn@teko.vn>
2023-11-07 16:44:19 +0700

    fix datetime extensions

--- app/helpers/time_int.py
+++ app/helpers/time_int.py
@@ -6,17 +6,17 @@ def now_int():
     return int(time.mktime(datetime.now().timetuple()))
 
 
-def time_int_short(reset_day: int, dt: datetime):
+def time_int_short(reset_day: int, dt: datetime = None):
     new_dt = dt or datetime.now()
     return int(new_dt.strftime("%Y%m%d%H")) + (reset_day - 1) * 100
 
 
-def time_int_short_day(dt: datetime):
+def time_int_short_day(dt: datetime = None):
     new_dt = dt or datetime.now()
     return int(new_dt.strftime("%Y%m%d")) * 100
 
 
-def time_int_day(dt: datetime):
+def time_int_day(dt: datetime = None):
     new_dt = dt or datetime.now()
     return int(new_dt.strftime("%Y%m%d"))
 
